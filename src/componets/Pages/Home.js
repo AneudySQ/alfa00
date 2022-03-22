928438efb07be791a4937dcf266bd6aa45de0a0d
AneudySQ <aneudysq@gmai.com>
2022-03-22 19:28:12 -0400

    trabajando con las hojas de estilos, creo que bootstrap no esta

--- src/componets/Pages/Home.js
+++ src/componets/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from '../HeroSection';
+import Toprestaurantes from '../Toprestaurantes';
 
 function Home() {
     return (
         <>
             <HeroSection />
+            <Toprestaurantes />
         </>
     );
 }
---
+++ src/componets/Toprestaurantes.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import '../scss/app.scss';
+
+
+function Toprestaurantes() {
+  return (
+    <>
+          <div clasName="white_bg">
+              <div clasName="container margin_60">
+                  <div clasName="main_title">
+                      <h2 clasName="nomargin_top">Choose from Most Popular</h2>
+                      <p>
+                          Cum doctus civibus efficiantur in imperdiet deterruisset.
+                      </p>
+                  </div>
+                  <div clasName="row">
+                      <div clasName="col-lg-6">
+                          <a href="detail_page.html" clasName="strip_list">
+                              <div clasName="ribbon_1">Popular</div>
+                              <div clasName="desc">
+                                  <div clasName="thumb_strip">
+                                      <img src="img/thumb_restaurant.jpg" alt=""/>
+                                  </div>
+                                  <div clasName="rating">
+                                      <i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star"></i>
+                                  </div>
+                                  <h3>Taco Mexican</h3>
+                                  <div clasName="type">
+                                      Mexican / American
+                                  </div>
+                                  <div clasName="location">
+                                      135 Newtownards Road, Belfast, BT4. <span clasName="opening">Opens at 17:00</span>
+                                  </div>
+                                  <ul>
+                                      <li>Take away<i clasName="icon_check_alt2 ok"></i></li>
+                                      <li>Delivery<i clasName="icon_check_alt2 ok"></i></li>
+                                  </ul>
+                              </div>
+                          </a>
+                          <a href="detail_page.html" clasName="strip_list">
+                              <div clasName="ribbon_1">Popular</div>
+                              <div clasName="desc">
+                                  <div clasName="thumb_strip">
+                                      <img src="img/thumb_restaurant_2.jpg" alt=""/>
+                                  </div>
+                                  <div clasName="rating">
+                                      <i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star"></i>
+                                  </div>
+                                  <h3>Naples Pizza</h3>
+                                  <div clasName="type">
+                                      Italian / Pizza
+                                  </div>
+                                  <div clasName="location">
+                                      135 Newtownards Road, Belfast, BT4. <span clasName="opening">Opens at 17:00</span>
+                                  </div>
+                                  <ul>
+                                      <li>Take away<i clasName="icon_check_alt2 ok"></i></li>
+                                      <li>Delivery<i clasName="icon_check_alt2 ok"></i></li>
+                                  </ul>
+                              </div>
+                          </a>
+                          <a href="detail_page.html" clasName="strip_list">
+                              <div clasName="ribbon_1">Popular</div>
+                              <div clasName="desc">
+                                  <div clasName="thumb_strip">
+                                      <img src="img/thumb_restaurant_3.jpg" alt=""/>
+                                  </div>
+                                  <div clasName="rating">
+                                      <i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star"></i>
+                                  </div>
+                                  <h3>Japan Food</h3>
+                                  <div clasName="type">
+                                      Sushi / Japanese
+                                  </div>
+                                  <div clasName="location">
+                                      135 Newtownards Road, Belfast, BT4. <span clasName="opening">Opens at 17:00</span>
+                                  </div>
+                                  <ul>
+                                      <li>Take away<i clasName="icon_check_alt2 ok"></i></li>
+                                      <li>Delivery<i clasName="icon_check_alt2 ok"></i></li>
+                                  </ul>
+                              </div>
+                          </a>
+                      </div>
+                      <div clasName="col-lg-6">
+                          <a href="detail_page.html" clasName="strip_list">
+                              <div clasName="ribbon_1">Popular</div>
+                              <div clasName="desc">
+                                  <div clasName="thumb_strip">
+                                      <img src="img/thumb_restaurant_4.jpg" alt=""/>
+                                  </div>
+                                  <div clasName="rating">
+                                      <i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star"></i>
+                                  </div>
+                                  <h3>Sushi Gold</h3>
+                                  <div clasName="type">
+                                      Sushi / Japanese
+                                  </div>
+                                  <div clasName="location">
+                                      135 Newtownards Road, Belfast, BT4. <span clasName="opening">Opens at 17:00</span>
+                                  </div>
+                                  <ul>
+                                      <li>Take away<i clasName="icon_check_alt2 ok"></i></li>
+                                      <li>Delivery<i clasName="icon_close_alt2 no"></i></li>
+                                  </ul>
+                              </div>
+                          </a>
+                          <a href="detail_page.html" clasName="strip_list">
+                              <div clasName="ribbon_1">Popular</div>
+                              <div clasName="desc">
+                                  <div clasName="thumb_strip">
+                                      <img src="img/thumb_restaurant_5.jpg" alt=""/>
+                                  </div>
+                                  <div clasName="rating">
+                                      <i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star"></i>
+                                  </div>
+                                  <h3>Dragon Tower</h3>
+                                  <div clasName="type">
+                                      Chinese / Thai
+                                  </div>
+                                  <div clasName="location">
+                                      135 Newtownards Road, Belfast, BT4. <span clasName="opening">Opens at 17:00</span>
+                                  </div>
+                                  <ul>
+                                      <li>Take away<i clasName="icon_check_alt2 ok"></i></li>
+                                      <li>Delivery<i clasName="icon_check_alt2 ok"></i></li>
+                                  </ul>
+                              </div>
+
+                          </a>
+                          <a href="detail_page.html" clasName="strip_list">
+                              <div clasName="ribbon_1">Popular</div>
+                              <div clasName="desc">
+                                  <div clasName="thumb_strip">
+                                      <img src="img/thumb_restaurant_6.jpg" alt=""/>
+                                  </div>
+                                  <div clasName="rating">
+                                      <i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star voted"></i><i clasName="icon_star"></i>
+                                  </div>
+                                  <h3>China Food</h3>
+                                  <div clasName="type">
+                                      Chinese / Vietnam
+                                  </div>
+                                  <div clasName="location">
+                                      135 Newtownards Road, Belfast, BT4. <span clasName="opening">Opens at 17:00</span>
+                                  </div>
+                                  <ul>
+                                      <li>Take away<i clasName="icon_check_alt2 ok"></i></li>
+                                      <li>Delivery<i clasName="icon_check_alt2 ok"></i></li>
+                                  </ul>
+                              </div>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+    </>
+  )
+}
+
+export default Toprestaurantes
