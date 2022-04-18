4d6f2d096fdb450e6f301fa7c28483c65b1628b9
AneudySQ <aneudysq@gmai.com>
2022-04-18 17:59:14 -0400

    trabajando en el boton

--- src/componets/AgregarCategoriaMenu.jsx
+++ src/componets/AgregarCategoriaMenu.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 
 function AgregarCategoriaMenu() {
+
+    const [agregarCategoria, setAgregarCategoria] = React.useState('')
+
+    const agregarCategoriaBtn = e => {
+        e.preventDefault()
+        console.log(agregarCategoria)
+        if(!agregarCategoria.trim()){
+            console.log('Elelemto Vacio')
+        }
+        console.log(agregarCategoria)
+    }
+
     return (
         <>
-            <div className="form-group row">
+            <form className="form-group row" onSubmit={agregarCategoriaBtn}>
                 <label>Menu Category</label>
                 <input 
-                type="text" 
-                name="menu_category" 
+                type='text'
                 className="form-control col-6" 
-                placeholder="EX. Starters" /> 
+                placeholder="Ingrese una categoria" 
+                onChange={ e => setAgregarCategoria(e.target.Value) }
+                /> 
 
-                <div 
-                className="col-3 btn_1 margin ml-2 "
-                >Agregar Categoria</div>
-            </div>
+                <button 
+                className="btn_1 col-3 left mx-2"
+                type='submit'
+                >
+                Agregar Categoria
+                </button>
+            </form >
         </>
     )
 }
