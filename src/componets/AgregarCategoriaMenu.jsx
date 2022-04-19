6aba4f2e067823ecd63321615d78c702c4e323d7
AneudySQ <aneudysq@gmail.com>
2022-04-18 22:36:50 -0400

    el boton esta ready, Yupiiii

--- src/componets/AgregarCategoriaMenu.jsx
+++ src/componets/AgregarCategoriaMenu.jsx
@@ -7,9 +7,10 @@ function AgregarCategoriaMenu() {
     const agregarCategoriaBtn = e => {
         e.preventDefault()
         console.log(agregarCategoria)
-        if(!agregarCategoria.trim()){
+        if (!agregarCategoria.trim()) {
             console.log('Elelemto Vacio')
         }
+        
         console.log(agregarCategoria)
     }
 
@@ -17,18 +18,19 @@ function AgregarCategoriaMenu() {
         <>
             <form className="form-group row" onSubmit={agregarCategoriaBtn}>
                 <label>Menu Category</label>
-                <input 
-                type='text'
-                className="form-control col-6" 
-                placeholder="Ingrese una categoria" 
-                onChange={ e => setAgregarCategoria(e.target.Value) }
-                /> 
 
-                <button 
-                className="btn_1 col-3 left mx-2"
-                type='submit'
+                <input
+                    type="text"
+                    className="form-control col-6"
+                    placeholder="Ingrese una categoria"
+                    onChange={e => setAgregarCategoria(e.target.value)}
+                />
+
+                <button
+                    className="btn_1 col-3 left mx-2"
+                    type="submit"
                 >
-                Agregar Categoria
+                    Agregar Categoria
                 </button>
             </form >
         </>
