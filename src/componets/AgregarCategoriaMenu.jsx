44ee8b5cf4c96f8adc5d05fe557c937183fc9f3a
AneudySQ <aneudysq@gmail.com>
2022-04-23 21:56:13 -0400

    apliacion terminada

--- src/componets/AgregarCategoriaMenu.jsx
+++ src/componets/AgregarCategoriaMenu.jsx
@@ -1,40 +1,81 @@
-import React from 'react'
+import React from "react";
 //import MenuItemCategoria from "./MenuItemCategoria";
-import shortid from 'shortid'
-
+import shortid from "shortid";
 
 function AgregarCategoriaMenu() {
 
     const [agregarCategoria, setAgregarCategoria] = React.useState('')
     const [itemMenu, setItemMenu] = React.useState([])
+    const [modoEdicion, setModoEdicion] = React.useState(false)
+    const [id, setId] = React.useState('')
+    const [error, setError] = React.useState(null)
 
     const agregarCategoriaBtn = e => {
         e.preventDefault()
         console.log(agregarCategoria)
         if (!agregarCategoria.trim()) {
-            console.log('Elelemto Vacio')
+            console.log("Elelemto Vacio")
+            setError('Escribe el nombre de la categoria')
+            return;
         }
+        console.log(agregarCategoria);
 
-        console.log(agregarCategoria)
+        setAgregarCategoria('');
 
         setItemMenu([
             ...itemMenu,
-            { id: shortid.generate(), nombreMenu: agregarCategoria }
+            { itemMenu, id: shortid.generate(), nombreCategoria: agregarCategoria }
         ])
+        setError(null)
     }
 
-    const eliminarCategoria = id =>{
-        console.log(id)
+    const eliminarCategoria = (id) => {
+        console.log(id);
 
-        const arrayFiltrado = itemMenu.filter(item => item.id !== id)
-        setItemMenu(arrayFiltrado)
+        const arrayFiltrado = itemMenu.filter((item) => item.id !== id);
+        setItemMenu(arrayFiltrado);
+    };
+
+    const editar = item => {
+        console.log(item)
+        setModoEdicion(true)
+        setAgregarCategoria(item.nombreCategoria)
+        setId(item.id)
+    }
+    const editarNombre = e => {
+        e.preventDefault()
+        if (!agregarCategoria.trim()) {
+            console.log("Elelemto Vacio")
+            setError('Escribe el nombre de la categoria')
+            return
+        }
+
+        const arrayEditado = itemMenu.map(
+            item => item.id === id ? { id, nombreCategoria: agregarCategoria }
+                : item
+        )
+
+        setItemMenu(arrayEditado)
+        setModoEdicion(false)
+        setAgregarCategoria('')
+        setId('')
 
     }
+
 
     return (
         <>
-            <form className="form-group row" onSubmit={agregarCategoriaBtn}>
-                <label>Menu Category</label>
+            {
+                error ? <span className="text-danger">{error}</span> : null
+            }
+
+            <form className="form-group row" onSubmit={modoEdicion ? editarNombre : agregarCategoriaBtn}>
+                <label>
+                    {
+                        modoEdicion ? (<h5 className=" text-warning">Editar Nombre de la categoria</h5>) : (<h5 className="text-success">Agregar Categoria</h5>)
+                    }
+                </label>
+
 
                 <input
                     type="text"
@@ -44,43 +85,54 @@ function AgregarCategoriaMenu() {
                     value={agregarCategoria}
                 />
 
-                <button
-                    className="btn_1 col-3 left mx-2"
-                    type="submit"
-                >
-                    Agregar Categoria
-                </button>
-            </form >
+                {
+                    modoEdicion ? (
+                        <button className="btn-warning col-3 left mx-2 " type="submit" >Editar nombre</button>
+
+                    ) : (
+                        <button className="btn_1 col-3 left mx-2 " type="submit" >Agregar Categoria</button>
+
+                    )
+
+                }
+
+
+            </form>
 
             {
-                itemMenu.map(item => (
+                itemMenu.length === 0 ?(
 
-                        <div className="menu-item-section clearfix">
-                            <h4>Menu item #1</h4>
-                            <div>
-                                <a href="#0">
-                                    <i className="icon_plus_alt"></i>
-                                </a>
+                    <h1 className="text-center text-secondary">No hay categorias</h1>
+                ) :(
 
-                                <a href="#0">
-                                    <button 
-                                    className="icon_minus_alt"
-                                    onClick={() => eliminarCategoria(item.id)}
+                        itemMenu.map((item) => (
+            <li className="menu-item-section clearfix" key={item.id}>
+                <h4>{item.nombreCategoria}</h4>
+                <div>
 
-                                    >
-                                              
-                                    </button>
-                                </a>
-                            </div>
-                        </div>
-                ))
+                    <a href="#0">
+                        <button
+                            className="btn btn-warning "
+                            onClick={() => editar(item)}
+                        >
+                            Editar
+                        </button>
+                    </a>
 
+                    <a href="#0">
+                        <button
+                            className="btn btn-danger "
+                            onClick={() => eliminarCategoria(item.id)}
+                        >Eliminar categoria</button>
+                    </a>
+                </div>
+            </li>
+            ))
+            )
+            
             }
-
-
-
         </>
-    )
+    );
 }
 
-export default AgregarCategoriaMenu
+export default AgregarCategoriaMenu;
