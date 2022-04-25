e770800ec3f19d7ddced089d5af4bda6ca35299a
AneudySQ <aneudysq@gmail.com>
2022-04-24 22:44:18 -0400

    subiendo para tirarme a dormir

--- src/componets/AgregarCategoriaMenu.jsx
+++ src/componets/AgregarCategoriaMenu.jsx
@@ -83,6 +83,7 @@ function AgregarCategoriaMenu(props) {
                 setAgregarCategoriaTarget={e => setAgregarCategoria(e.target.value)}
                 value={agregarCategoria}
             />
+            
 
             {
                 itemMenu.length === 0 ? (
--- src/componets/Pages/App0.jsx
+++ src/componets/Pages/App0.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Saludo from '../Saludo'
+import { Accordion } from 'react-bootstrap'
 
 
 const App0 = (props) => {
@@ -15,6 +16,32 @@ const App0 = (props) => {
               <Saludo persona='Adrian' />
               <Saludo persona='Lucas' />
           </div>
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+            est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+            est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
 
     </div>
   )
